4a11465f653ab1f875b579088f5d1ca2463ac49a
stephani <stephani.hc.97@gmail.com>
2025-03-21 22:38:44 -0400

    adicion del dockerfile

---
+++ NutritionalKitchen-Backend/NutritionalKitchen.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionalKitchen.WebApi.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect("/swagger");
+        }
+    }
+} 
